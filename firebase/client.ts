3821946e096dce623112112da43d083e55920bdf
hiimanshu26 <himanshuyadav26010@gmail.com>
2026-01-05 05:11:58 +0530

    setup app, file and folder structure, routing and auth pages

---
+++ firebase/client.ts
@@ -0,0 +1,18 @@
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCyD7hmtMf8fma3Yrgg0NOUppbM9IZI478",
+    authDomain: "preppilot-47e7e.firebaseapp.com",
+    projectId: "preppilot-47e7e",
+    storageBucket: "preppilot-47e7e.firebasestorage.app",
+    messagingSenderId: "606455459592",
+    appId: "1:606455459592:web:0040ac2cc683069a1fc6bd",
+    measurementId: "G-NLVQ4D44ZQ"
+};
+
+const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+
+export const auth = getAuth(app);
+export const db = getFirestore(app)
